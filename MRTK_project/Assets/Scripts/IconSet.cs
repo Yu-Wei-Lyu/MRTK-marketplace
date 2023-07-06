35a08adc614d2b52e57062e0277d2da92aa60e8c
Yu-Wei, Lyu <awaia732@gmail.com>
2023-07-06 21:24:39 +0800

    IconSet create

---
+++ MRTK_project/Assets/Scripts/IconSet.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconNameSet
+{
+    public const string LightbulbOff = "LightbulbOff";
+
+    public const string LightbulbOn = "LightbulbOn";
+}
