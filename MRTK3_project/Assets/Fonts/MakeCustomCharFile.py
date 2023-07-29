0d75785e62094ce28c83b06f4929b9b56508b997
Yu-Wei, Lyu <awaia732@gmail.com>
2023-07-29 23:22:47 +0800

    MRTK3  (TextMeshPro) character display bug fixed (MSJH.asset) => (MSJH-Chinese.asset)

---
+++ MRTK3_project/Assets/Fonts/MakeCustomCharFile.py
@@ -0,0 +1,15 @@
+'''
+Unicode區段查詢 https://zh.wikipedia.org/zh-tw/Unicode%E5%8D%80%E6%AE%B5
+'''
+
+unicode_hex_ranges = [
+    range(0x0020, 0x007E + 1),  # 基本拉丁字母 可字化範圍 (Ascii)
+    range(0x4E00, 0x9FFF + 1),  # 中日韓統一表意文字 (基本區)
+    range(0xFF00, 0xFFEF + 1)   # 半形及全形字元
+]
+merge_set = set().union(*unicode_hex_ranges)
+with open('MakeCustomCharFileOutput.txt', 'w', encoding='utf-8') as file:
+    for codepoint in merge_set:
+        char = chr(codepoint)
+        file.write(char)
+print("所選Unicode範圍已寫入檔案 'MakeCustomCharFileOutput.txt' 中。")
