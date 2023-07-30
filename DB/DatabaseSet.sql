ee162764237bc6c8391fd8a890198acabe56b903
SNWBS <75601692+SNWBS@users.noreply.github.com>
2023-07-30 22:37:34 +0800

    Database_Script_update

--- DB/DatabaseSet.sql
+++ DB/DatabaseSet.sql
@@ -2,13 +2,16 @@ show databases;
 CREATE DATABASE IF NOT EXISTS mydatabase;
 
 USE mydatabase;
-USE mysql;
-Drop database mysql;
 
-Drop Table furniture;
-Drop Table Order_items;
+Drop Table Furniture;
 
 show tables;
+
+Drop Table users;
+Drop Table orders;
+Drop Table order_items;
+Drop Table history;
+Drop Table furniture;
 
 -- 建立家具表格
 CREATE TABLE Furniture (
@@ -20,44 +23,32 @@ CREATE TABLE Furniture (
   Size VARCHAR(20),
   Description VARCHAR(100),
   Material VARCHAR(50),
-  manufacturers VARCHAR(100) NULL
+  manufacturers VARCHAR(50)
 );
 
 ALTER TABLE Furniture
 ADD ImagePath VARCHAR(255);
 
-SELECT User, Host FROM mydatabase.user;
-
-GRANT ALL PRIVILEGES ON mydatabase.* TO '109590037'@'118.150.125.153';
-
-ALTER USER '109590037'@'118.150.125.153' IDENTIFIED WITH mysql_native_password BY '109590037';
-
-CREATE USER 'project'@'118.150.125.153' IDENTIFIED BY 'project';
-
-ALTER TABLE users
-ADD COLUMN manufacturers VARCHAR(100) NULL;
-
-ALTER USER '109590037'@'118.150.125.153' IDENTIFIED WITH mysql_native_password BY '109590037';
-
-SELECT user, host, plugin FROM mysql.user WHERE user = '109590037';
-
-ALTER TABLE History MODIFY HistoryID INT AUTO_INCREMENT;
+ALTER TABLE History MODIFY HistoryID INT PRIMARY KEY AUTO_INCREMENT;
 
 ALTER TABLE Furniture MODIFY ID INT AUTO_INCREMENT PRIMARY KEY;
 
+ALTER TABLE Furniture MODIFY manufacturers varchar(50);
+
+
 -- 插入家具資料
-INSERT INTO Furniture (Name, Number, Price, ImagePath, Size, Description, Material, manufacturers)
+INSERT INTO Furniture (Name, Number, Price, ImagePath, Size, Description, Material)
 VALUES
-  ('椅子', 'C001', 1000, 'chair.jpg', '60x60x80', '舒適的布藝椅子', '布料、木材','GreenTech Co.'),
-  ('沙發', 'S001', 5000, 'sofa.jpg', '180x80x70', '現代風格皮革沙發', '皮革、鋼架','SilverStone Manufacturing'),
-  ('書桌', 'D001', 800, 'desk.jpg', '120x60x75', '簡約風格書桌', '木材','GreenTech Co.'),
-  ('床架', 'B001', 3000, 'bed.jpg', '160x200', '現代風格床架', '木材、金屬','GreenTech Co.'),
-  ('餐桌', 'T001', 1500, 'dining_table.jpg', '120x80x75', '實木餐桌', '木材','BlueWave Products'),
-  ('衣櫃', 'W001', 2000, 'wardrobe.jpg', '180x60x200', '現代風格衣櫃', '木材、鏡子','SilverStone Manufacturing'),
-  ('電視櫃', 'TV001', 1200, 'tv_stand.jpg', '160x40x50', '簡約風格電視櫃', '木材、玻璃','GreenTech Co.'),
-  ('書架', 'B002', 600, 'bookshelf.jpg', '80x30x180', '經典木質書架', '木材','BlueWave Products'),
-  ('檯燈', 'L001', 200, 'desk_lamp.jpg', '30x30x50', '現代風格檯燈', '金屬、塑膠','BlueWave Products'),
-  ('茶几', 'C002', 800, 'coffee_table.jpg', '80x80x40', '實木茶几', '木材','GreenTech Co.');
+  ('椅子', 'C001', 1000, 'chair.jpg', '60x60x80', '舒適的布藝椅子', '布料、木材'),
+  ('沙發', 'S001', 5000, 'sofa.jpg', '180x80x70', '現代風格皮革沙發', '皮革、鋼架'),
+  ('書桌', 'D001', 800, 'desk.jpg', '120x60x75', '簡約風格書桌', '木材'),
+  ('床架', 'B001', 3000, 'bed.jpg', '160x200', '現代風格床架', '木材、金屬'),
+  ('餐桌', 'T001', 1500, 'dining_table.jpg', '120x80x75', '實木餐桌', '木材'),
+  ('衣櫃', 'W001', 2000, 'wardrobe.jpg', '180x60x200', '現代風格衣櫃', '木材、鏡子'),
+  ('電視櫃', 'TV001', 1200, 'tv_stand.jpg', '160x40x50', '簡約風格電視櫃', '木材、玻璃'),
+  ('書架', 'B002', 600, 'bookshelf.jpg', '80x30x180', '經典木質書架', '木材'),
+  ('檯燈', 'L001', 200, 'desk_lamp.jpg', '30x30x50', '現代風格檯燈', '金屬、塑膠'),
+  ('茶几', 'C002', 800, 'coffee_table.jpg', '80x80x40', '實木茶几', '木材');
 
 -- 創建 users 表格
 CREATE TABLE users (
@@ -119,17 +110,35 @@ END //
 DELIMITER ;
 
 -- 插入帳號
-INSERT INTO users (username, password, role, manufacturers)
-VALUES  ('JohnDoe', 'password1', 'member','GreenTech Co.'),
-		('JaneSmith', 'password2', 'member','SilverStone Manufacturing'),
-		('MikeJohnson', 'password3', 'member','SilverStone Manufacturing'),
-		('EmilyBrown', 'password4', 'member','GreenTech Co.'),
-		('DavidLee', 'password5', 'member','BlueWave Products');
-
-DELETE FROM users
-WHERE role = 'member'
-LIMIT 10;
+INSERT INTO users (username, password, role)
+VALUES  ('109590037', '109590037', 'admin'),
+		('109590004', '109590004', 'admin'),
+		('109590003', '109590003', 'admin'),
+        ('JohnDoe', 'password1', 'member'),
+		('JaneSmith', 'password2', 'member'),
+		('MikeJohnson', 'password3', 'member'),
+		('EmilyBrown', 'password4', 'member'),
+		('DavidLee', 'password5', 'member');
 
 Select * from Furniture;
+SELECT * FROM furniture;
+select * from order_items;
 Select * from history;
 Select * from users;
+
+SHOW PROCESSLIST;
+kill 8;
+kill 18;
+kill 21;
+kill 23;
+kill 24;
+kill 25;
+kill 26;
+kill 27;
+kill 28;
+kill 30;
+kill 31;
+kill 32;
+kill 33;
+kill 37;
+kill 17;
