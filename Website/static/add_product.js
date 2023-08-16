bccfc74314af72740bfb3b219ac4b923ccb81ca2
Glenn Wang <abc20200130abc@gmail.com>
2023-08-17 01:06:23 +0800

    add_product.html input check

--- Website/static/add_product.js
+++ Website/static/add_product.js
@@ -1,4 +1,6 @@
-$('#add').click(
+
+// jQuery add
+/* $('#add').click(
     function(){
         var request_data = new FormData();
         var Title = $("#title").val()
@@ -56,7 +58,7 @@ $('#add').click(
             }
         }
     }
-);
+);*/
 
 function myFunction() {
     var text = "確定要登出?";
@@ -65,8 +67,93 @@ function myFunction() {
     }
 }
 
-function displayFileName() {
-    const fileInput = document.getElementById("picture");
-    const fileNameSpan = document.getElementById("fileName");
-    fileNameSpan.textContent = fileInput.files[0]?.name || "";
+function displayPictureName() {
+    const PictureInput = document.getElementById("picture");
+    const PictureNameSpan = document.getElementById("pictureName");
+    PictureNameSpan.textContent = PictureInput.files[0]?.name || "";
 }
+
+function displayModelName() {
+    const ModelInput = document.getElementById("model");
+    const ModelNameSpan = document.getElementById("modelName");
+    ModelNameSpan.textContent = ModelInput.files[0]?.name || "";
+}
+
+const unicode_hex_ranges = [
+    Array.from(range(0x0020, 0x007E + 1)),
+    Array.from(range(0x3000, 0x303F + 1)),
+    Array.from(range(0x4E00, 0x9FFF + 1)),
+    Array.from(range(0xFF00, 0xFFEF + 1))
+  ];
+
+  function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx);
+  }
+
+  function checkInput(inputElement) {
+    const inputValue = inputElement.value;
+
+    // 檢查輸入是否包含不允許的字符或 &
+    const hasInvalidChars = inputValue.split('').some(char => {
+      return !unicode_hex_ranges.flat().includes(char.charCodeAt(0)) || char === '&';
+    });
+
+    // 根據檢查結果顯示或隱藏錯誤訊息
+    const errorElement = document.getElementById('inputError');
+    errorElement.style.display = hasInvalidChars ? 'inline-block' : 'none';
+  }
+  
+  function handleAddClick() {
+    const fields = [
+      { id: 'title', name: '名稱' },
+      { id: 'number', name: '編號' },
+      { id: 'price', name: '售價' },
+      { id: 'depth', name: '深度'},
+      { id: 'width', name: '寬度'},
+      { id: 'height', name: '高度'},
+      { id: 'material', name: '材質'},
+      { id: 'description', name: '描述'},
+      // 添加其他欄位...
+    ];
+  
+    let hasError = false;
+    let invalidCharsText = '';
+  
+    fields.forEach(field => {
+      const inputElement = document.getElementById(field.id);
+      const inputValue = inputElement.value;
+      const invalidChars = [];
+  
+      inputValue.split('').forEach(char => {
+        if (!unicode_hex_ranges.flat().includes(char.charCodeAt(0)) || char === '&') {
+          invalidChars.push(char);
+          hasError = true;
+        }
+      });
+  
+      if (invalidChars.length > 0) {
+        const errorElement = document.getElementById('inputError');
+        errorElement.style.display = 'inline-block';
+  
+        const fieldErrorText = `${field.name}欄位中包含不允許的字符: ${invalidChars.join(', ')}`;
+        if (invalidCharsText === '') {
+          invalidCharsText = fieldErrorText;
+        } else {
+          invalidCharsText += `<br>${fieldErrorText}`;
+        }
+      }
+    });
+  
+    if (hasError) {
+      const errorElement = document.getElementById('inputError');
+      errorElement.innerHTML = invalidCharsText; // 使用 innerHTML 插入換行
+    } else {
+      // 執行上架動作
+      // TODO: 在這裡執行上架的相關操作
+      console.log('商品已上架');
+      
+      // 清除錯誤訊息
+      const errorElement = document.getElementById('inputError');
+      errorElement.style.display = 'none';
+    }
+  }
