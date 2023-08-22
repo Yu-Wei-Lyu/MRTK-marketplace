4434b87b517931bb8e854d27fd21934b32975b4f
Glenn Wang <abc20200130abc@gmail.com>
2023-08-22 16:12:30 +0800

    Added frontend example with simulated backend data and integrated WebSocket functionality.

--- Website/static/edit_product.js
+++ Website/static/edit_product.js
@@ -1,134 +1,271 @@
-$.ajax({
-    type: "POST",
-    url: "edit_product_load",
-    dataType: "json",
-    success: function(response_data){
-        Object.keys(response_data).forEach(function(doc){
-            // console.log(response_data[doc][0]);
-            
-            var id = doc;
-            var title = response_data[doc][0];
-            var number = response_data[doc][1];
-            var price = response_data[doc][2];
-            var picture = response_data[doc][3];
-            var depth = response_data[doc][4];
-            var width = response_data[doc][5];
-            var height = response_data[doc][6];
-            var quality = response_data[doc][7];
-            var description = response_data[doc][8];
+const socket = new WebSocket('ws://118.150.125.153:8765');
 
-            $("#title").val(title);
-            $("#number").val(number);
-            $("#price").val(price);
-            $("#picture").val(picture);
-            $("#depth").val(depth);
-            $("#width").val(width);
-            $("#height").val(height);
-            $("#quality").val(quality);
-            $("#description").val(description);
-            
-            const col = `
-                <tr>
-                  <td id="Title">${title}</td>
-                  <td>${number}</td>
-                  <td>${price}</td>
-                  <td>${picture}</td>
-                  <td>${depth}</td>
-                  <td>${width}</td>
-                  <td>${height}</td>
-                  <td>${quality}</td>
-                  <td>${description}</td>
-                </tr>`;
-            $("#product_field").append(col)
+// 當接收到訊息時更新網頁內容
+socket.onmessage = function(event) {
+    try {
+        const data = JSON.parse(event.data);
+        const message_type = data.type;
+        const resultsList = document.getElementById('results');
+        resultsList.innerHTML = data.message[2];
+        
+        /*if (message_type === 'query') {
+            const imageData = data.message;
+            imageData_string = imageData.substring(2, imageData.length - 2);
+            imageData_string = imageData_string.split(',')
+            imageData_string = imageData_string[9]
+
+            // 解碼 Base64 資料為 Uint8Array
+            const decodedData = atob(imageData_string);
+
+            // 將 Uint8Array 轉換為 Uint8Array 視圖
+            const uint8Array = new Uint8Array(decodedData.length);
+            for (let i = 0; i < decodedData.length; i++) {
+                uint8Array[i] = decodedData.charCodeAt(i);
+            }
+
+            // 建立 Blob 物件
+            const blob = new Blob([uint8Array], { type: 'image/jpeg' });
+
+            // 建立 URL 物件，並設置圖片的 Blob URL 作為 src
+            const imageUrl = URL.createObjectURL(blob);
+            const imageDisplay = document.getElementById('imageDisplay');
+            imageDisplay.src = imageUrl;
+        }*/
+        
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+    }
+};
+
+// 點擊按鈕時發送訊息給伺服器
+function sendQuery() {
+  console.log("SendQuery");
+  socket.send('{"type":"query"}');
+};
+
+// 點擊按鈕時向伺服器發送更新資料的請求
+function updateData() {
+    console.log("SendUpdate");
+    
+    const imageInput = document.getElementById('imageInput');
+    const selectedImage = imageInput.files[0];
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const imageData = event.target.result;
+        // 從輸入欄位獲取資料
+        const name = document.getElementById('title').value;
+        // const number = document.getElementById('number').value;
+        
+        const price = document.getElementById('price').value;
+        // const imagePath = document.getElementById('imagePathInput').value;
+        
+        // 取得家具size
+        const depth = document.getElementById('depth').value;
+        const width = document.getElementById('width').value;
+        const height = document.getElementById('height').value;
+        const size = depth + 'x' + width + 'x' + height;
+
+        // 取得分類標籤
+        const selectedCategories = [];
+        const checkboxes = document.querySelectorAll('input[name="category"]:checked');
+        checkboxes.forEach(function(checkbox) {
+            selectedCategories.push(checkbox.value);
         });
-    }
-});
+        // console.log('選中的分類：', selectedCategories);
 
-$('#remove').click(
-    function(){
-        var request_data = new FormData();
-        var Title = $("#Title").text();
-        if(confirm('確定要下架【' + Title + '】這件商品嗎?') == true){
-            $.ajax({
-                type: "POST",
-                url: "remove_product_send",
-                data: request_data,
-                success: function(response_data){
-                    
-                },
-                dataType: "json",
-                contentType: false,
-                processData: false
-            });
-            alert("成功下架【" + Title + "】!")
-            window.location.href='/admin';
-        }
-    }
-);
+        const description = document.getElementById('description').value;
+        const material = document.getElementById('material').value;
 
-
-$('#save').click(
-    function(){
-        var request_data = new FormData();
-        var Title = $("#Title").text();
-        request_data.append('title',$("#title").val());
-        request_data.append('number',$("#number").val());
-        request_data.append('price',$("#price").val());
-        request_data.append('picture',$("#picture").val());
-        request_data.append('depth',$("#depth").val());
-        request_data.append('width',$("#width").val());
-        request_data.append('height',$("#height").val());
-        request_data.append('quality',$("#quality").val());
-        request_data.append('description',$("#description").val());
         
-        // console.log($("#title").val())
-        // console.log($("#count").val())
-        // console.log($("#isbn").val())
-        // console.log($("#author").val())
-        
-        if ($("#title").val() === ""){
-            alert("【名稱】未輸入!")
+
+        // 從輸入欄位獲取要更新的資料的 ID
+        const updateId = document.getElementById('updateIdInput').value;
+
+        // 建立要傳送的資料物件
+        const requestData = {
+            type: 'update',
+            id: updateId,
+            Name: name,
+            // Number: number, 應該可移除
+            Price: price,
+            // ImagePath: imagePath, 應該可移除
+            Size: size,
+            Tags:selectedCategories,
+            Description: description,
+            Material: material,
+            // Manufacturer: manufacturer, 不知加在哪  
+            ImageUrl: imgurImageUrl, // Add Imgur Image URL
+            // ModelUrl: imgurModelUrl, // Add Imgur Model URL 未實裝
+        };
+    
+        // 將資料物件轉成 JSON 格式
+        const jsonRequestData = JSON.stringify(requestData);
+    
+        // 發送資料給伺服器
+        socket.send(jsonRequestData);
+    };
+    reader.readAsDataURL(selectedImage); // 读取图像文件为Base64编码
+}
+
+ // 點擊按鈕時向伺服器發送刪除資料的請求
+ function deleteData() {
+    console.log("SendDelete");
+    // 從輸入欄位獲取要刪除的資料的 ID
+    const deleteId = document.getElementById('deleteIdInput').value;
+
+    // 建立要傳送的資料物件
+    const requestData = {
+        type: 'delete',
+        id: deleteId
+    };
+
+    // 將資料物件轉成 JSON 格式
+    const jsonRequestData = JSON.stringify(requestData);
+
+    // 發送資料給伺服器
+    socket.send(jsonRequestData);
+}
+
+function myFunction() {
+    var text = "確定要登出?";
+    if (confirm(text) == true) {
+      window.location.href = "/";
+    }
+}
+
+function displayPictureName() {
+    const PictureInput = document.getElementById("picture");
+    const PictureNameSpan = document.getElementById("pictureName");
+    PictureNameSpan.textContent = PictureInput.files[0]?.name || "";
+}
+
+function displayModelName() {
+    const ModelInput = document.getElementById("model");
+    const ModelNameSpan = document.getElementById("modelName");
+    ModelNameSpan.textContent = ModelInput.files[0]?.name || "";
+}
+
+const unicode_hex_ranges = [
+    Array.from(range(0x0020, 0x007E + 1)),
+    Array.from(range(0x3000, 0x303F + 1)),
+    Array.from(range(0x4E00, 0x9FFF + 1)),
+    Array.from(range(0xFF00, 0xFFEF + 1))
+];
+
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx);
+}
+  
+function handleAddClick() {
+  const fields = [
+    { id: 'title', name: '名稱', maxLength: 50 }, // 最大 50 個字
+    { id: 'price', name: '售價', maxLength: 10 }, // 最大 10 個字
+    { id: 'depth', name: '深度', maxLength: 9 }, // 最大 9 個字
+    { id: 'width', name: '寬度', maxLength: 9 }, // 最大 9 個字
+    { id: 'height', name: '高度', maxLength: 9 }, // 最大 9 個字
+    { id: 'material', name: '材質', maxLength: 50 }, // 最大 50 個字
+    { id: 'description', name: '描述', maxLength: 100 }, // 最大 100 個字
+    // 添加其他欄位...
+  ];
+
+  let hasError = false;
+  let invalidCharsText = '';
+
+  fields.forEach(field => {
+    const inputElement = document.getElementById(field.id);
+    const inputValue = inputElement.value.trim(); // 去掉首尾空白
+    const invalidChars = [];
+
+    if (!inputValue) {
+        hasError = true;
+        const fieldErrorText = `${field.name}為必填欄位`;
+        if (invalidCharsText === '') {
+            invalidCharsText = fieldErrorText;
+        } else {
+            invalidCharsText += `<br>${fieldErrorText}`;
         }
-        else if ($("#number").val() === ""){
-            alert("【編號】未輸入!")
-        }
-        else if ($("#price").val() === ""){
-            alert("【售價】未輸入!")
-        }
-        else if ($("#picture").val() === ""){
-            alert("【圖片】未上傳!")
-        }
-        else if ($("#depth").val() === ""){
-            alert("【深度】未輸入!")
-        }
-        else if ($("#width").val() === ""){
-            alert("【寬度】未輸入!")
-        }
-        else if ($("#height").val() === ""){
-            alert("【高度】未輸入!")
-        }
-        else if ($("#quality").val() === ""){
-            alert("【材質】未輸入!")
-        }
-        else if ($("#description").val() === ""){
-            alert("【描述】未輸入!")
-        }
-        else{
-            if(confirm('確定要儲存【' + Title + '】商品的編輯動作嗎?') == true){
-                $.ajax({
-                    type: "POST",
-                    url: "save_edit_product_send",
-                    data: request_data,
-                    success: function(response_data){
-                        
-                    },
-                    dataType: "json",
-                    contentType: false,
-                    processData: false
-                });
-                alert("成功編輯【" + Title + "】!")
-                window.location.href='/admin';
+    } else {
+        inputValue.split('').forEach(char => {
+            if (!unicode_hex_ranges.flat().includes(char.charCodeAt(0)) || char === '&') {
+                invalidChars.push(char);
+                hasError = true;
+            }
+        });
+
+        if (inputValue.length > field.maxLength) {
+            hasError = true;
+            const fieldErrorText = `${field.name}欄位超過字數限制，最多允許${field.maxLength}個字`;
+            if (invalidCharsText === '') {
+                invalidCharsText = fieldErrorText;
+            } else {
+                invalidCharsText += `<br>${fieldErrorText}`;
+            }
+        } else if (invalidChars.length > 0) {
+            const fieldErrorText = `${field.name}欄位中包含不允許的字符: ${invalidChars.join(', ')}`;
+            if (invalidCharsText === '') {
+                invalidCharsText = fieldErrorText;
+            } else {
+                invalidCharsText += `<br>${fieldErrorText}`;
             }
         }
     }
-);
+  });
+  
+  const pictureInput = document.getElementById('picture');
+  const modelInput = document.getElementById('model');
+
+  if (!pictureInput.files.length) {
+      hasError = true;
+      const fieldErrorText = '請選擇圖片';
+      if (invalidCharsText === '') {
+          invalidCharsText = fieldErrorText;
+      } else {
+          invalidCharsText += `<br>${fieldErrorText}`;
+      }
+  }
+
+  if (!modelInput.files.length) {
+      hasError = true;
+      const fieldErrorText = '請選擇模型';
+      if (invalidCharsText === '') {
+          invalidCharsText = fieldErrorText;
+      } else {
+          invalidCharsText += `<br>${fieldErrorText}`;
+      }
+  }
+
+  const categoryCheckboxes = document.querySelectorAll('input[name="category"]');
+  let categorySelected = false;
+
+  categoryCheckboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+          categorySelected = true;
+      }
+  });
+
+  if (!categorySelected) {
+      hasError = true;
+      const fieldErrorText = '請選擇至少一個分類';
+      if (invalidCharsText === '') {
+          invalidCharsText = fieldErrorText;
+      } else {
+          invalidCharsText += `<br>${fieldErrorText}`;
+      }
+  }
+
+  if (hasError) {
+    const errorElement = document.getElementById('inputError');
+    errorElement.innerHTML = invalidCharsText; // 使用 innerHTML 插入換行
+    errorElement.style.display = 'inline-block'; // 顯示錯誤訊息
+  } else {
+    // 執行上架動作
+    // TODO: 在這裡執行上架的相關操作
+    addData();
+    console.log('商品已上架');
+    
+    // 清除錯誤訊息
+    const errorElement = document.getElementById('inputError');
+    errorElement.style.display = 'none';
+  }
+}
--- Website/static/product_detail.js
+++ Website/static/product_detail.js
@@ -1,6 +1,97 @@
-function myFunction() {
-    var text = "確定要登出?";
-    if (confirm(text) == true) {
-      window.location.href = "/";
+const socket = new WebSocket('ws://118.150.125.153:8765');
+
+// 當接收到訊息時更新網頁內容
+socket.onmessage = function(event) {
+    try {
+        const data = JSON.parse(event.data);
+        const message_type = data.type;
+        const resultsList = document.getElementById('results');
+        resultsList.innerHTML = data.message[2];
+        
+        /*if (message_type === 'query') {
+            const imageData = data.message;
+            imageData_string = imageData.substring(2, imageData.length - 2);
+            imageData_string = imageData_string.split(',')
+            imageData_string = imageData_string[9]
+
+            // 解碼 Base64 資料為 Uint8Array
+            const decodedData = atob(imageData_string);
+
+            // 將 Uint8Array 轉換為 Uint8Array 視圖
+            const uint8Array = new Uint8Array(decodedData.length);
+            for (let i = 0; i < decodedData.length; i++) {
+                uint8Array[i] = decodedData.charCodeAt(i);
+            }
+
+            // 建立 Blob 物件
+            const blob = new Blob([uint8Array], { type: 'image/jpeg' });
+
+            // 建立 URL 物件，並設置圖片的 Blob URL 作為 src
+            const imageUrl = URL.createObjectURL(blob);
+            const imageDisplay = document.getElementById('imageDisplay');
+            imageDisplay.src = imageUrl;
+        }*/
+        
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
     }
+};
+
+// 點擊按鈕時發送訊息給伺服器
+function sendQuery() {
+  console.log("SendQuery");
+  socket.send('{"type":"query"}');
+};
+
+ // 點擊按鈕時向伺服器發送刪除資料的請求
+ function deleteData() {
+  console.log("SendDelete");
+  // 從輸入欄位獲取要刪除的資料的 ID
+  const deleteId = document.getElementById('deleteIdInput').value;
+
+  // 建立要傳送的資料物件
+  const requestData = {
+      type: 'delete',
+      id: deleteId
+  };
+
+  // 將資料物件轉成 JSON 格式
+  const jsonRequestData = JSON.stringify(requestData);
+
+  // 發送資料給伺服器
+  socket.send(jsonRequestData);
+}
+
+// 假設你從後端獲取的 JSON 數據長這樣
+const productData = {
+  title: "商品名稱",
+  category: ["書房．辦公家具","收納用品","哭阿測試"],
+  price: "99",
+  depth: "10 cm",
+  width: "20 cm",
+  height: "30 cm",
+  material: "木頭，其實是鐵",
+  description: "這是一個描述商品的範例文字。",
+  imageURL: "ProductImages/Example2.jpg", //暫時連結
+  modelURL: "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoomBox/glTF-Binary/BoomBox.glb"
+};
+
+// 使用獲取到的數據填充 HTML 元素的內容
+document.getElementById("productTitle").textContent = productData.title;
+document.getElementById("category").textContent = `分類：${productData.category.join("、")}`;
+document.getElementById("price").textContent = `售價：NT$ ${productData.price}`;
+document.getElementById("depth").textContent = `深度:${productData.depth}`;
+document.getElementById("width").textContent = `寬度:${productData.width}`;
+document.getElementById("height").textContent = `高度:${productData.height}`;
+document.getElementById("material").textContent = `材質:${productData.material}`;
+document.getElementById("description").textContent = `描述：${productData.description}`;
+document.getElementById("image").src = productData.imageURL;
+document.getElementById("model").href = productData.modelURL;
+
+// 登出問題
+function myFunction() {
+  var text = "確定要登出?";
+  if (confirm(text) == true) {
+    window.location.href = "/";
+  }
 }
